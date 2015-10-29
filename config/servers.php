09bba46f84d32013255d7b9d6bb7c19d13ce40ff
Filip Wójciak <fwojciak@ganymede.eu>
2015-10-29 15:12:31 +0100

    Better info in example servers.php

--- config/servers.php
+++ config/servers.php
@@ -1,15 +1,19 @@
 <?php
 
-$hint = 'Configure servers in servers.php config file';
+# See http://docs.phpmyadmin.net/en/latest/config.html#server-connection-settings for advanced options.
 
-$i;
+$hint = 'See instructions in README to configure list of servers.';
+
+$i = 1;
 $cfg['Servers'][$i]['host'] = 'localhost';
 $cfg['Servers'][$i]['verbose'] = $hint;
 
 ++$i;
-$cfg['Servers'][$i]['host'] = 'localhost';
+$cfg['Servers'][$i]['host'] = '192.168.0.1';
 $cfg['Servers'][$i]['verbose'] = 'Server 1.';
+$cfg['Servers'][$i]['port'] = '3307';
 
 ++$i;
-$cfg['Servers'][$i]['host'] = 'localhost';
+$cfg['Servers'][$i]['host'] = '192.168.0.2';
 $cfg['Servers'][$i]['verbose'] = 'Server 2.';
+$cfg['Servers'][$i]['port'] = '3308';
